4a5578082e9359ddbc50c91fe982acff23067e22
Matteo Valentini <matteo.valentini@student.tugraz.at>
2024-10-22 13:25:57 +0200

    changed header title

--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -1,7 +1,7 @@
 const AppTitle = () => {
     return (
         <div className="row-start-1 col-start-1 flex justify-center items-center bg-gradient-to-r from-cyan-400 to-cyan-500 bg-clip-text">
-            <span className="text-transparent text-2xl font-bold">ICE Timeplanner SS 24</span>
+            <span className="text-transparent text-2xl font-bold">ICE Timeplanner WS 24/25</span>
         </div>
     )
 }
